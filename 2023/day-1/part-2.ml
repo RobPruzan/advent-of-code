2c3dc47f1970edf166d13ef161ed922054c6c5be
RobPruzan <rapruzan@buffalo.edu>
2023-12-02 14:24:32 -0500

    day 1 part 2

--- 2023/day-1/part-2.ml
+++ 2023/day-1/part-2.ml
@@ -1,31 +1,47 @@
 let is_digit c = c >= '0' && c <= '9'
 (*
-   - over each line, we need to do a sliding window from left to end
-   - we make that a new seq/list of digits
-   - get the head and tail
-   - then just same thing as before
+  - over each line, we need to do a sliding window from left to end
+  - we make that a new seq/list of digits
+  - get the head and tail
+  - then just same thing as before
 *)
 
+type digit_item = {value: int; index: int}
+
+type temp_char = {value: char; index: int}
+
+let digit_of_head_and_last list =
+  match list with
+  | [] -> 0  (* Empty list case *)
+  | [x] -> (int_of_string (String.make 1 x.value ^ String.make 1 x.value))
+  | head :: _ as full_list -> 
+      let rec find_last = function
+        | [] -> failwith "wadu"
+        | [last] -> last
+        | _ :: tail -> find_last tail
+      in
+      let last = find_last full_list in
+    int_of_string ((String.make 1 head.value) ^ (String.make 1 last.value))
 let get_digit s =
   match s with
   | "one" ->
-      Some 1
+      Some '1'
   | "two" ->
-      Some 2
+      Some '2'
   | "three" ->
-      Some 3
+      Some '3'
   | "four" ->
-      Some 4
+      Some '4'
   | "five" ->
-      Some 5
+      Some '5'
   | "six" ->
-      Some 6
+      Some '6'
   | "seven" ->
-      Some 7
+      Some '7'
   | "eight" ->
-      Some 8
+      Some '8'
   | "nine" ->
-      Some 9
+      Some '9'
   | _ ->
       None
 
@@ -36,6 +52,7 @@ let slice_it i j seq = Seq.(seq |> drop i |> take (j - i + 1))
 let read_file filename =
   let in_channel = open_in filename in
   let sum = ref 0 in
+  
   try
     while true do
       let line = in_channel |> input_line |> String.to_seq in
@@ -43,19 +60,28 @@ let read_file filename =
       let left = ref 0 in
       let right = ref 0 in
       let acc = ref [] in
+      let mapped_line =
+        List.mapi (fun idx el -> {value= el; index= idx}) (List.of_seq line)
+      in
+      let filtered_line = List.filter (fun x -> is_digit x.value) mapped_line in
+      acc := List.append filtered_line !acc ;
       while !left < Seq.length line do
         let curr = slice_it !left !right line |> join |> get_digit in
-        Option.iter (fun inside -> acc := List.append [inside] !acc) curr ;
-        (* right := !right + 1 *)
-        if Seq.length line - 1 >= !right then left := !left + 1 ;
-        right := !left ;
-        ()
-      done
+        Option.iter
+          (fun inside -> acc := List.append [{value= inside; index= !left}] !acc)
+          curr ;
+        
+        if !right >= Seq.length line - 1 then (
+          left := !left + 1 ;
+          right := !left )
+        else right := !right + 1
+      done;
+      sum:= !sum + digit_of_head_and_last (List.sort (fun a b -> a.index - b.index) !acc)
+      
+      
     done
   with End_of_file ->
-    (* End_of_file exception is raised when we reach the end of the file *)
     print_int !sum ; print_newline () ; close_in in_channel
-(* Close the file when we're done *)
 ;;
 
 read_file "day-1-input.txt"
